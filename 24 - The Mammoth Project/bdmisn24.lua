22338bb46399b48af805172adb8d409307e6976a
BlackDragonN001 <rebelion2@rock.com>
2016-10-21 22:44:00 -0500

    -Renamed all mission files, bumping numeric count from 20-29, to 21-30 for consistency. -Added special assets for BDOG mission 6, 7, and 10. -Changed APCs in BDOG 6 to Tugs.

---
+++ 24 - The Mammoth Project/bdmisn24.lua
@@ -0,0 +1,173 @@
+-- Battlezone: Rise of the Black Dogs, Black Dog Mission 24 written by General BlackDragon.
+
+local M = {
+-- Bools
+UpdateObjectives = false,
+
+StartDone = false, -- Some things don't work in the actual "Start" function.
+Nav1Reached = false, -- Are we at nav1 yet? 
+DecoyTriggered = false, -- It's a Trap!
+TrapEscaped = false, -- Whew, close one!
+MammothStolen = false, -- Steal the Mammoth!
+MammothDead = false, -- You can't kill something that's extinct.
+MissionOver = false, -- Yay!
+
+-- Floats (realy doubles in Lua)
+
+-- Handles
+Player = nil,
+Nav = { },
+Mammoth = nil,
+MammothDecoy = nil,
+
+-- Ints
+Aud1 = 0
+}
+
+function Save()
+    return 
+		M
+end
+
+function Load(...)	
+    if select('#', ...) > 0 then
+		M
+		= ...
+    end
+end
+
+function Start()
+
+    print("Black Dog Mission 24 Lua created by General BlackDragon");
+
+end
+
+function AddObject(h)
+
+	local Team = GetTeamNum(h);
+
+end
+
+function DeleteObject(h)
+
+	
+end
+
+function Update()
+	
+	M.Player = GetPlayerHandle();
+	
+	if not M.StartDone then
+		
+		M.Mammoth = GetHandle("mammoth");
+		SetIndependence(M.Mammoth, 0); -- Nope.
+		M.MammothDecoy = GetHandle("badmammoth");
+		SetIndependence(M.MammothDecoy, 0); -- Nope.
+
+		-- In BZ64, navs are invincible, maybe keep it that way for now. (units in this mission use navs to goto)
+		for i = 1, 2 do
+			M.Nav[i] = GetHandle("nav" .. i);
+			if i == 2 then
+				SetObjectiveName(M.Nav[i], "Pickup Zone");
+			else
+				SetObjectiveName(M.Nav[i], "Navpoint 1");
+			end
+			SetMaxHealth(M.Nav[i], 0);
+		end
+			
+		--[[ -- Units already set in BZN to do patrol. :)
+		for i = 1, 2 do
+			Patrol(GetHandle("patrol1_" .. i), "patrol_1", 1);
+			Patrol(GetHandle("patrol2_" .. i),  "patrol_2", 1);
+			Patrol(GetHandle("patrol3_" .. i),  "patrol_3", 1);
+		--]]
+		
+		M.StartDone = true;
+		
+		-- Start up the mission.
+		M.Aud1 = AudioMessage("bdmisn2401.wav");
+		M.UpdateObjectives = true;
+		SetObjectiveOn(M.Nav[1]);
+	end
+	
+	-- Handle Objectives.
+	if M.UpdateObjectives then
+	
+		ClearObjectives();
+		M.UpdateObjectives = false;
+		
+		if not M.MammothDead then
+			-- If you haven't stolen anything yet, you're a good boy.
+			if not M.MammothStolen then
+				-- First order, investigate the Mammoth.
+				if not M.DecoyTriggered then
+					AddObjective("bdmisn2401.otf", "WHITE");
+				else
+					-- but bad news!
+					if not M.TrapEscaped then
+						AddObjective("bdmisn2402.otf", "WHITE");
+					else -- Go find me a Mammoth.
+						AddObjective("bdmisn2402.otf", "GREEN");
+						AddObjective("bdmisn2403.otf", "WHITE");
+					end
+				end		
+			else -- Get to the drop zone.
+				AddObjective("bdmisn2404.otf", "WHITE");
+			end
+		else -- ITS DEAD! NOOOOO! NEW Fail objective. -GBD
+			AddObjective("bdmisn2405.otf", "RED");
+		end
+	end
+	
+	-- You're there, but it's a trap!
+	if not M.Nav1Reached and GetDistance(M.Player, M.Nav[1]) < 175.0 then
+		Attack(BuildObject("svhraz", 2, "spawn_svhraz1"), M.Player);
+		Attack(BuildObject("svhraz", 2, "spawn_svhraz2"), M.Player);
+		Attack(BuildObject("svfigh", 2, "spawn_svfigh1"), M.Player);
+		Attack(BuildObject("svfigh", 2, "spawn_svfigh2"), M.Player);
+		Attack(BuildObject("svrckt", 2, "spawn_svrkct1"), M.Player);
+		Attack(BuildObject("svrckt", 2, "spawn_svrckt2"), M.Player);
+		M.Nav1Reached = true;
+		M.UpdateObjectives = true;
+	end
+	-- IMPROVEMENT? Move spawn up to here, closer to when u get to mammoth decoy?
+	if not M.DecoyTriggered and GetDistance(M.Player, M.MammothDecoy) < 150.0 then
+		Damage(M.MammothDecoy, 90001); -- It's over 9000!!!
+		M.Aud1 = AudioMessage("bdmisn2402.wav");
+		M.DecoyTriggered = true;
+	end
+	-- Okay, your safe.
+	if M.DecoyTriggered and not M.TrapEscaped and GetDistance(M.Player, M.Nav[1]) > 400.0 then
+		M.Aud1 = AudioMessage("bdmisn2403.wav");
+		SetObjectiveOff(M.Nav[1]);
+		SetObjectiveOn(M.Mammoth);
+		M.TrapEscaped = true;
+		M.UpdateObjectives = true;
+	end
+	
+	-- Did you do the deed yet?
+	if not M.MammothStolen and M.Player == M.Mammoth then
+		M.Aud1 = AudioMessage("bdmisn2404.wav");
+		SetObjectiveOff(M.Nav[1]);
+		SetObjectiveOff(M.Mammoth);
+		SetObjectiveOn(M.Nav[2]);
+		M.MammothStolen = true;
+		M.UpdateObjectives = true;
+	end
+	
+	-- Win Conditions:
+	if not M.MissionOver and M.MammothStolen and M.Player == M.Mammoth and GetDistance(M.Player, M.Nav[2]) < 50.0 then
+		SucceedMission(GetTime()+5.0, "bdmisn24wn.des");
+		M.MissionOver = true;
+		M.UpdateObjectives = true;
+	end
+	
+	-- Lose Conditions
+	if not M.MissionOver and not IsValid(M.Mammoth) then
+		FailMission(GetTime()+5.0, "bdmisn24l1.des");
+		M.MammothDead = true;
+		M.MissionOver = true;
+		M.UpdateObjectives = true;
+	end
+	
+end
---
+++ 25 - The Last Stand/bdmisn25.lua
@@ -0,0 +1,165 @@
+-- Battlezone: Rise of the Black Dogs, Black Dog Mission 25 written by General BlackDragon.
+
+local M = {
+-- Bools
+UpdateObjectives = false,
+
+StartDone = false, -- Some things don't work in the actual "Start" function.
+WalkerDead = false, -- Did it go poof?
+SecondAttack = false, -- Better stay or prepare for this one.
+SovDefenseSpawn = false, -- It's under attack, defend it!
+SovietCommDead = false, -- Is it dead yet?
+CommDead = false; -- Did you lose your toy?
+MissionOver = false, -- Yay!
+
+-- Floats (realy doubles in Lua)
+SecondWave = 0,
+
+-- Handles
+Player = nil,
+CommTower = nil,
+SovietComm = nil,
+Walker = nil,
+
+-- Ints
+Aud1 = 0
+}
+
+function Save()
+    return 
+		M
+end
+
+function Load(...)	
+    if select('#', ...) > 0 then
+		M
+		= ...
+    end
+end
+
+function Start()
+
+    print("Black Dog Mission 25 Lua created by General BlackDragon");
+
+end
+
+function AddObject(h)
+
+	local Team = GetTeamNum(h);
+
+end
+
+function DeleteObject(h)
+
+	
+end
+
+function Update()
+	
+	M.Player = GetPlayerHandle();
+	
+	if not M.StartDone then
+		
+		M.CommTower = GetHandle("commtower");
+		M.SovietComm = GetHandle("sovietcomm");
+		M.Walker = GetHandle("george");
+
+		for i = 1, 13 do
+			Patrol(GetHandle("patrol_" .. i), "patrol_path", 1);
+		end
+		
+		for i = 1, 8 do
+			if i == 2 then
+				Attack(GetHandle("attacker_" .. i), M.Walker, 1);
+			else
+				Attack(GetHandle("attacker_" .. i), M.CommTower, 1);
+			end
+		end
+		
+		M.StartDone = true;
+		
+		-- Start up the mission.
+		M.Aud1 = AudioMessage("bdmisn2501.wav");
+		M.UpdateObjectives = true;
+		SetObjectiveOn(M.CommTower);
+		SetObjectiveOn(M.SovietComm);
+		Goto(M.Walk, "walker_path", 0);
+		M.SecondWave = GetTime()+135.0; -- 2 minutes 15 seconds later, spawn 2nd attack wave.
+	end
+	
+	-- Handle Objectives.
+	if M.UpdateObjectives then
+	
+		ClearObjectives();
+		M.UpdateObjectives = false;
+		
+		if not M.WalkerDead then
+			-- Did you lose your base?
+			if not M.CommDead then
+				AddObjective("bdmisn2501.otf", "WHITE");
+			else
+				AddObjective("bdmisn2501.otf", "RED");
+			end
+			
+			-- You maanged to win without losing the walker, good job.
+			if not M.SovietCommDead then
+				AddObjective("bdmisn2502.otf", "WHITE");
+			else
+				AddObjective("bdmisn2502.otf", "GREEN");
+			end
+		else
+			if not M.SovietCommDead then
+				AddObjective("bdmisn2503.otf", "WHITE");
+			else
+				AddObjective("bdmisn2503.otf", "GREEN");
+			end
+		end
+	end
+	
+	-- Add Health to the SovietComm so it's harder to kill? IMPROVEMENT: Just set it's health to 20,000 instead. GBD
+	--[[
+	if GetFrame / GetTimeStep() then
+		AddHealth(M.SovietComm, 100);
+	end
+	--]]
+	
+	-- If the walker dies, urge up the Attack.
+	if not M.WalkerDead and not IsValid(M.Walker) then
+		M.Aud1 = AudioMessage("bdmisn2502.wav");
+		M.WalkerDead = true;
+		M.UpdateObjectives = true;
+	end
+	
+	-- Spawn the 2nd (and currently only) additional attack wave.
+	if not M.SecondAttack and GetTime() > M.SecondWave then
+		for i = 1, 4 do
+			Attack(BuildObject("svfigh", 2, "patrol_path"), M.CommTower);
+		end
+		M.SecondAttack = true;	
+	end
+	
+	-- Spawn CommTower defenses.
+	if not M.SovDefenseSpawn and GetWhoShotMe(M.SovietComm) ~= 0 then
+		Patrol(BuildObject("svfigh", 2, "defense_spawn"), "defense_path");
+		Patrol(BuildObject("svfigh", 2, "defense_spawn"), "defense_path");
+		Patrol(BuildObject("svltnk", 2, "defense_spawn"), "defense_path");
+		M.SovDefenseSpawn = true;
+	end
+	
+	-- Win Conditions:
+	if not M.MissionOver and not IsAlive(M.SovietComm) then
+		SucceedMission(GetTime()+5.0, "bdmisn25wn.des");
+		M.SovietCommDead = true;
+		M.MissionOver = true;
+		M.UpdateObjectives = true;
+	end
+	
+	-- Lose Conditions
+	if not M.MissionOver and not IsAlive(M.CommTower) then
+		FailMission(GetTime()+5.0, "bdmisn25l1.des");
+		M.CommDead = true;
+		M.MissionOver = true;
+		M.UpdateObjectives = true;
+	end
+	
+end
