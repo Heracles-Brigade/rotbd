f98f4e2506931e8f8c3e5d76f18d114150a4bcb0
Seqan01 <sdbell92794@gmail.com>
2017-08-07 00:50:27 -0500

    Covert Ops fixes included, including new HG2 and LGT files as well as reworked BZN.
    Des files in RBDNew03 fixed because of copy/pasta ' issues.
    
    Lua rewrite partially complete.

--- improved/RBDnew03/rbdnew03.lua
+++ improved/RBDnew03/rbdnew03.lua
@@ -1,4 +1,4 @@
--- Battlezone: Rise of the Black Dogs, Black Dog Mission 24 written by General BlackDragon.
+-- Battlezone: Rise of the Black Dogs Redux Mission 3 coded by Seqan and Vemahk
 
 
 require("bz_logging");
@@ -22,11 +22,10 @@ MammothDead = false, -- You can't kill something that's extinct.
 DropZoneReached = false, -- Are we there yet?
 MissionOver = false, -- Yay!
 
--- Floats (realy doubles in Lua)
-
 -- Handles
 Player = nil,
-Nav = { },
+NavCoords = { },
+ObjectiveNav = nil,
 Mammoth = nil,
 MammothDecoy = nil,
 
@@ -59,7 +58,7 @@ end
 
 function Start()
 	bzCore:onStart();
-	print("Black Dog Mission 24 Lua created by General BlackDragon");
+	print("Black Dog Mission 3 Lua created by Seqan and Vemahk");
 end
 
 function AddObject(h)
@@ -85,6 +84,56 @@ function afterLoad()
 	loaded = false;
 end
 
+local function UpdateObjectives() -- Handle Objectives.
+	ClearObjectives();
+	
+	if not M.MammothDead then
+		-- If you haven't stolen anything yet, you're a good boy.
+		if not M.MammothStolen then
+			-- First order, investigate the Mammoth.
+			if not M.DecoyTriggered then
+				AddObjective("rbdnew0301.otf", "WHITE");
+			else
+				-- but bad news!
+				if not M.TrapEscaped then
+					AddObjective("rbdnew0302.otf", "WHITE");
+				else -- Go find me a Mammoth.
+					AddObjective("rbdnew0302.otf", "GREEN");
+					AddObjective("rbdnew0303.otf", "WHITE");
+				end
+			end		
+		else -- Get to the drop zone.
+			if not M.DropZoneReached then
+				AddObjective("rbdnew0304.otf", "WHITE");
+			else
+				AddObjective("rbdnew0304.otf", "GREEN");
+			end
+		end
+	else -- ITS DEAD! NOOOOO! NEW Fail objective. -GBD
+		AddObjective("rbdnew0305.otf", "RED");
+	end
+end
+
+local function SpawnNav(num)
+	local nav = BuildObject("apcamr", 1, M.NavCoords[num]);
+	SetLabel(nav, "nav"..num);
+	
+	if num == 2 then
+		SetName(nav, "Pickup Site");
+	else
+		SetName(nav, "Nav "..num);
+	end
+	
+	SetMaxHealth(nav, 0); -- Can't go boom-boom.
+	
+	-- Switches the active objective from the old nav to the new nav.
+	if M.ObjectiveNav then
+		SetObjectiveOff(M.ObjectiveNav);
+	end
+	SetObjectiveOn(nav);
+	M.ObjectiveNav = nav; -- Sets the new nav to the ObjectiveNav so that the next time this function is called, it can switch off of it.
+end
+
 function Update(dtime)
 	bzCore:update(dtime);
 	
@@ -103,65 +152,21 @@ function Update(dtime)
 
 		-- In BZ64, navs are invincible, maybe keep it that way for now. (units in this mission use navs to goto)
 		for i = 1, 2 do
-			M.Nav[i] = GetHandle("nav" .. i);
-			if i == 2 then
-				SetObjectiveName(M.Nav[i], "Pickup Zone");
-			else
-				SetObjectiveName(M.Nav[i], "Navpoint 1");
-			end
-			SetMaxHealth(M.Nav[i], 0);
+			local tmpnav = GetHandle("nav" .. i);
+			M.NavCoords[i] = GetPosition(tmpnav);
+			RemoveObject(tmpnav);
 		end
-			
-		--[[ -- Units already set in BZN to do patrol. :)
-		for i = 1, 2 do
-			Patrol(GetHandle("patrol1_" .. i), "patrol_1", 1);
-			Patrol(GetHandle("patrol2_" .. i),  "patrol_2", 1);
-			Patrol(GetHandle("patrol3_" .. i),  "patrol_3", 1);
-		--]]
 		
 		M.StartDone = true;
 		
 		-- Start up the mission.
-		M.Aud1 = AudioMessage("bdmisn2401.wav");
-		M.UpdateObjectives = true;
-		SetObjectiveOn(M.Nav[1]);
-	end
-	
-	-- Handle Objectives.
-	if M.UpdateObjectives then
-	
-		ClearObjectives();
-		M.UpdateObjectives = false;
-		
-		if not M.MammothDead then
-			-- If you haven't stolen anything yet, you're a good boy.
-			if not M.MammothStolen then
-				-- First order, investigate the Mammoth.
-				if not M.DecoyTriggered then
-					AddObjective("bdmisn2401.otf", "WHITE");
-				else
-					-- but bad news!
-					if not M.TrapEscaped then
-						AddObjective("bdmisn2402.otf", "WHITE");
-					else -- Go find me a Mammoth.
-						AddObjective("bdmisn2402.otf", "GREEN");
-						AddObjective("bdmisn2403.otf", "WHITE");
-					end
-				end		
-			else -- Get to the drop zone.
-				if not M.DropZoneReached then
-					AddObjective("bdmisn2404.otf", "WHITE");
-				else
-					AddObjective("bdmisn2404.otf", "GREEN");
-				end
-			end
-		else -- ITS DEAD! NOOOOO! NEW Fail objective. -GBD
-			AddObjective("bdmisn2405.otf", "RED");
-		end
+		M.Aud1 = AudioMessage("rbdnew0301.wav");
+		UpdateObjectives();
+		SpawnNav(1);
 	end
 	
 	-- You're there, but it's a trap!
-	if not M.Nav1Reached and GetDistance(M.Player, M.Nav[1]) < 175.0 then
+	if not M.Nav1Reached and GetDistance(M.Player, M.ObjectiveNav) < 175.0 then
 		Attack(BuildObject("svhraz", 2, "spawn_svhraz1"), M.Player);
 		Attack(BuildObject("svhraz", 2, "spawn_svhraz2"), M.Player);
 		Attack(BuildObject("svfigh", 2, "spawn_svfigh1"), M.Player);
@@ -169,48 +174,48 @@ function Update(dtime)
 		Attack(BuildObject("svrckt", 2, "spawn_svrkct1"), M.Player);
 		Attack(BuildObject("svrckt", 2, "spawn_svrckt2"), M.Player);
 		M.Nav1Reached = true;
-		M.UpdateObjectives = true;
+		UpdateObjectives();
 	end
+	
 	-- IMPROVEMENT? Move spawn up to here, closer to when u get to mammoth decoy?
-	if not M.DecoyTriggered and GetDistance(M.Player, M.MammothDecoy) < 150.0 then
-		Damage(M.MammothDecoy, 90001); -- It's over 9000!!!
-		M.Aud1 = AudioMessage("bdmisn2402.wav");
+	if not M.DecoyTriggered and IsWithin(M.Player, M.MammothDecoy, 150.0) then
+		BuildObject("xbmbxpl", 0, M.MammothDecoy);
+		Damage(M.MammothDecoy, 9001); -- It's over 9000!!!
+		M.Aud1 = AudioMessage("rbdnew0302.wav");
 		M.DecoyTriggered = true;
-		M.UpdateObjectives = true;
+		UpdateObjectives();
 	end
-	-- Okay, your safe.
-	if M.DecoyTriggered and not M.TrapEscaped and GetDistance(M.Player, M.Nav[1]) > 400.0 then
-		M.Aud1 = AudioMessage("bdmisn2403.wav");
-		SetObjectiveOff(M.Nav[1]);
+	
+	-- Okay, your (IT'S YOU'RE) safe.
+	if M.DecoyTriggered and not M.TrapEscaped and GetDistance(M.Player, M.ObjectiveNav) > 400.0 then
+		M.Aud1 = AudioMessage("rbdnew0303.wav");
 		SetObjectiveOn(M.Mammoth);
 		M.TrapEscaped = true;
-		M.UpdateObjectives = true;
+		UpdateObjectives();
 	end
 	
 	-- Did you do the deed yet?
 	if not M.MammothStolen and M.Player == M.Mammoth then
-		M.Aud1 = AudioMessage("bdmisn2404.wav");
-		SetObjectiveOff(M.Nav[1]);
+		M.Aud1 = AudioMessage("rbdnew0304.wav");
 		SetObjectiveOff(M.Mammoth);
-		SetObjectiveOn(M.Nav[2]);
+		SpawnNav(2);
 		M.MammothStolen = true;
-		M.UpdateObjectives = true;
+		UpdateObjectives();
 	end
 	
 	-- Win Conditions:
-	if not M.MissionOver and M.MammothStolen and M.Player == M.Mammoth and GetDistance(M.Player, M.Nav[2]) < 50.0 then
-		SucceedMission(GetTime()+5.0, "bdmisn24wn.des");
+	if not M.MissionOver and M.MammothStolen and M.Player == M.Mammoth and IsWithin(M.Player, M.Nav[2], 50.0) then
+		SucceedMission(GetTime()+5.0, "rbdnew03wn.des");
 		M.MissionOver = true;
-		M.UpdateObjectives = true;
 		M.DropZoneReached = true;
+		UpdateObjectives();
 	end
 	
 	-- Lose Conditions
 	if not M.MissionOver and not IsValid(M.Mammoth) then
-		FailMission(GetTime()+5.0, "bdmisn24l1.des");
+		FailMission(GetTime()+5.0, "rbdnew03l1.des");
 		M.MammothDead = true;
 		M.MissionOver = true;
-		M.UpdateObjectives = true;
+		UpdateObjectives();
 	end
-	
 end
