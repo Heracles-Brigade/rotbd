81ff8b958cfc915e973c0b46374f769f81b8893d
John 'Nielk1' Klein <development@nielk1.com>
2025-05-06 17:36:57 -0500

    remove test comments of flow, removed some dead code

--- src/improved/RBDnew01/rbdnew01.lua
+++ src/improved/RBDnew01/rbdnew01.lua
@@ -50,25 +50,6 @@ function RemoveConvoy(...)
     end
 end
 
-local function spawnNextNav()
-	-- todo can globrals.currentNav be null
-    local current = globals.currentNav;
-    --local nav = BuildObject("apcamr", 1, GetPathPoints("nav_path")[current]);
-
-    --SetObjectiveName(nav, ("Navpoint %d"):format(current));
-    --SetObjectiveOn(nav);
-    --local tmp = GameObject.FromHandle(nav);
-    local tmp = navmanager.BuildImportantNav(nil, 1, "nav_path", current - 1);
-    tmp:SetObjectiveName(("Navpoint %d"):format(current));
-    tmp:SetObjectiveOn();
-    local nav = tmp:GetHandle();
-
-    SetMaxHealth(nav, 0);
-    table.insert(globals.navs, nav);
-    globals.currentNav = globals.currentNav + 1;
-    return nav;
-end
-
 local function enemiesInRange(dist,place)
     local enemies_nearby = false;
     for v in ObjectsInRange(dist,place) do
@@ -106,10 +87,41 @@ local function createWave(odf, path_list, follow)
     return unpack(ret);
 end
 
---Define all objectives
+-- Define all objectives
+-- @todo does this work properly if the tug gets sniped? Oh, it's not snipable
+statemachine.Create("tug_relic_convoy",
+    function (state)
+        if GetCurrentCommand(state.tug) == AiCommand["NONE"] then
+            state:next();
+        end
+    end,
+    function (state)
+        Pickup(state.tug, state.relic);
+        state:next();
+    end,
+    function (state)
+        if GetCurrentCommand(state.tug) == AiCommand["NONE"] then
+            state:next();
+        end
+    end,
+    function (state)
+        Goto(state.tug, "leave_path");
+        state:next();
+    end,
+    function (state)
+        if GetCurrentCommand(state.tug) == AiCommand["NONE"] then
+            state:next();
+        end
+    end,
+    function (state)
+        RemoveObject(state.apc);
+        RemoveObject(state.relic);
+        state:next();
+    end);
+
 statemachine.Create("delayed_spawn",
     statemachine.SleepSeconds(120),
-    function(state)
+    function (state)
         createWave("svfigh",{"spawn_n1","spawn_n2"},"north_path");
         createWave("svtank",{"spawn_n3"},"north_path");
         state:next();
@@ -126,30 +138,29 @@ end
 
 
 statemachine.Create("main_objectives", {
-    { "start", function(state)
+    { "start", function (state)
         CameraReady();
         AudioMessage(audio.intro);
         state:next();
     end },
-    { "opening_cin", function(state)
+    { "opening_cin", function (state)
         if state:SecondsHavePassed(20) or CameraPath("opening_cin", 2000, 1000, globals.cafe) or CameraCancelled() then
             state:SecondsHavePassed(); -- clear timer if we got here without it being cleared
             CameraFinish();
             state:next();
         end
     end },
-    { "check_command_obj", function(state)
+    { "check_command_obj", function (state)
         state.nav1 = navmanager.BuildImportantNav(nil, 1, "nav_path", 0);
         state.nav1:SetMaxHealth(0);
         state.nav1:SetObjectiveName("Navpoint 1");
         state.nav1:SetObjectiveOn();
         AddObjective('bdmisn211.otf', "white");
         state.command = GetHandle("sbhqcp0_i76building");
-        print('bdmisn211.otf', state.command);
         state:next();
     end },
-    { "check_command_passfail", function(state)
-        --if GetDistance(GetPlayerHandle(), state.command) < 50.0 then
+    { "check_command_passfail", function (state)
+        if GetDistance(GetPlayerHandle(), state.command) < 50.0 then
             AudioMessage(audio.inspect);
             SetObjectiveOff(state.nav1);
             UpdateObjective('bdmisn211.otf',"green");
@@ -158,9 +169,9 @@ statemachine.Create("main_objectives", {
         --    UpdateObjective('bdmisn211.otf',"red");
         --    FailMission(GetTime() + 5,"bdmisn21ls.des");
         --    state:switch("end");
-        --end
+        end
     end },
-    { "destory_solar1_obj", function(state)
+    { "destory_solar1_obj", function (state)
         -- move this to configuration
         state.target_l1 = {"sbspow1_powerplant","sbspow2_powerplant","sbspow3_powerplant","sbspow4_powerplant"};
         state.target_l2 = {"sbspow7_powerplant","sbspow8_powerplant","sbspow5_powerplant","sbspow6_powerplant"};
@@ -176,14 +187,14 @@ statemachine.Create("main_objectives", {
         end
         state:next();
     end },
-   { "destory_solar1_pass", function(state)
-        --if(checkDead(state.handles)) then
+   { "destory_solar1_pass", function (state)
+        if(checkDead(state.handles)) then
             UpdateObjective('bdmisn212.otf',"green");
 			AudioMessage(audio.power1);
             state:next();
-        --end
+        end
     end },
-    { "destory_solar2_obj", function(state)
+    { "destory_solar2_obj", function (state)
         state.nav_solar1:SetObjectiveOff();
         state.nav_solar2 = navmanager.BuildImportantNav(nil, 1, "nav_path", 2);
         state.nav_solar2:SetMaxHealth(0);
@@ -196,27 +207,23 @@ statemachine.Create("main_objectives", {
         end
         state:next();
     end },
-    { "destory_solar2_pass", function(state)
-        --if(checkDead(state.handles)) then
+    { "destory_solar2_pass", function (state)
+        if(checkDead(state.handles)) then
             state.nav_solar2:SetObjectiveOff();
             UpdateObjective('bdmisn213.otf',"green");
             state:next();
-        --end
+        end
     end },
     { "destroy_solar_postgap", statemachine.SleepSeconds(3, "destroy_solar_success") },
-    { "destroy_solar_success", function(state)
+    { "destroy_solar_success", function (state)
         AudioMessage(audio.power2);
         state:next();
     end },
-    { "destroy_comm_start", function(state)
-
-        -- temp var creation
-        state.gotRelic = false;
-
-        state.nav_research = navmanager.BuildImportantNav(nil, 1, "nav_path", 3);
-        state.nav_research:SetMaxHealth(0);
-        state.nav_research:SetObjectiveName("Research Facility");
-        state.nav_research:SetObjectiveOn();
+    { "destroy_comm_start", function (state)
+        globals.nav_research = navmanager.BuildImportantNav(nil, 1, "nav_path", 3);
+        globals.nav_research:SetMaxHealth(0);
+        globals.nav_research:SetObjectiveName("Research Facility");
+        globals.nav_research:SetObjectiveOn();
 
         SetObjectiveOn(globals.comm);
         
@@ -230,26 +237,29 @@ statemachine.Create("main_objectives", {
         SetPilotClass(tug, ""); -- This is invincible.
         SetPilotClass(apc, ""); -- This is invincible.
         Follow(apc,tug);
-        local tugTasks = mission.TaskManager:sequencer(tug);
-        tugTasks:queue2("Pickup",globals.relic);
-        tugTasks:queue2("Goto","leave_path");
-        tugTasks:queue2("RemoveConvoy",apc,globals.relic);
-        Pickup(tug,globals.relic);
-        print("Pickup",tug,globals.relic);
+
+        -- attach values to the StateMachineIter so it can use them
+        tug_relic_convoy.tug = tug;
+        tug_relic_convoy.apc = apc;
+        tug_relic_convoy.relic = globals.relic;
+        globals.mission_states:on("tug_relic_convoy");
+
+        --Pickup(tug,globals.relic); -- this seems redundant
+
         Goto(BuildObject("avtank",2,"spawn_tank1"),globals.comm);
         Goto(BuildObject("avtank",2,"spawn_tank2"),globals.comm);
         Goto(BuildObject("avtank",2,"spawn_tank3"),globals.comm);
 
         state:next();
     end },
-    { "convoy_cin", function(state)
+    { "convoy_cin", function (state)
         if CameraPath("convoy_cin",2000,2000, globals.cafe) or CameraCancelled() then
             CameraFinish();
             state:next();
         end
     end },
-    { "destroy_obj", function(state)
-        --if not IsAlive(globals.comm) then
+    { "destroy_obj", function (state)
+        if not IsAlive(globals.comm) then
 
             UpdateObjective('bdmisn214.otf',"green");
             UpdateObjective('bdmisn215.otf',"green");
@@ -260,7 +270,7 @@ statemachine.Create("main_objectives", {
             
 
             state:next();
-        --end
+        end
     end },
     function (state)
         ClearObjectives();
@@ -274,7 +284,7 @@ statemachine.Create("main_objectives", {
         -- @todo make sure the state.nav_research detection works, since we are doing some wacky stuff in the back end
 
         --Only show if area is not cleared
-        if(enemiesInRange(270,state.nav_research)) then
+        if(enemiesInRange(270,globals.nav_research)) then
             state.research_enemies_still_exist = true;
             AddObjective("bdmisn311.otf","white");
     --      else --Removed due to redundancy
@@ -282,7 +292,7 @@ statemachine.Create("main_objectives", {
         end
         state:next();
     end,
-    statemachine.SleepSeconds(90, nil, function(state) return not enemiesInRange(270,state.nav) end),
+    statemachine.SleepSeconds(90, nil, function (state) return not enemiesInRange(270,state.nav) end),
     function (state)
         if state.research_enemies_still_exist then
             UpdateObjective("bdmisn311.otf","green");
@@ -308,7 +318,6 @@ statemachine.Create("main_objectives", {
         end
     end,
     function (state) -- success state
-        globals.keepGTsAtFullHealth = true;
         --Spawn in recycler
         --Recycler escort
 
@@ -342,152 +351,136 @@ statemachine.Create("main_objectives", {
         AddObjective('bdmisn2201.otf',"white");
         state:next();
     end },
-    function(state)
+    function (state)
         if(IsDeployed(GetRecyclerHandle(1))) then
             state:next();
-            --mission.Objective:Start(state.next)
         end
     end,
-    function(state)
+    function (state)
         UpdateObjective('bdmisn2201.otf',"green");
         ClearObjectives();
         
-        --mission.Objective:Start('make_scavs');
         state:next();
-        
-        --mission.Objective:Start('delayed_spawn');
         globals.mission_states:on("delayed_spawn");
     end,
-    { "make_scavs", function(state)
+    { "make_scavs", function (state)
         SetObjectiveOff(GetHandle("nav4"));
         AddObjective('bdmisn2202.otf',"white");
         state:next();
     end },
-    function(state)
+    function (state)
         --Check if player has 2 scavengers
         if(tracker:gotOfClass("scavenger",2)) then
             state:next();
         end
     end,
-    function(state)
+    function (state)
         UpdateObjective('bdmisn2202.otf',"green");
-        
-        --mission.Objective:Start('get_scrap');
         state:next();
     end,
-    { "get_scrap", function(state)
+    { "get_scrap", function (state)
         AddObjective('bdmisn2203.otf',"white");
         createWave("svtank",{"spawn_w1"},"west_path"); 
         createWave("svfigh",{"spawn_w4","spawn_w5"},"west_path");
         state:next();
     end },
-    function(state)
-        if(GetScrap(1) >= 20) then
+    function (state)
+        if GetScrap(1) >= 20 then
             state:next();
         end
     end,
-    function(state)
+    function (state)
         ClearObjectives();
-
-        --mission.Objective:Start('make_factory');
         state:next();
     end,
-    { "make_factory", function(state)
+    { "make_factory", function (state)
         AddObjective('bdmisn2204.otf',"white");
         state:next();
     end },
-    function(state)
-        if(tracker:gotOfClass("factory",1)) then
+    function (state)
+        if tracker:gotOfClass("factory",1) then
             state:next();
         end
     end,
-    function(state)
+    function (state)
         UpdateObjective('bdmisn2204.otf',"green");
-
-        --mission.Objective:Start('make_comm');
         state:next();
     end,
-    { "make_comm", function(state)
+    { "make_comm", function (state)
         AddObjective('bdmisn2209.otf',"white");
         createWave("svtank",{"spawn_w1"},"west_path"); 
         createWave("svfigh",{"spawn_w4","spawn_w5"},"west_path");
         state:next();
     end },
-    function(state)
+    function (state)
         if(tracker:gotOfClass("commtower",1)) then
             state:nextz();
         end
     end,
-    function(state)
+    function (state)
         UpdateObjective('bdmisn2209.otf',"green");
-        
-        --mission.Objective:Start('destroy_soviet');
         state:switch("destroy_soviet");
     end,
     
     -- SKIPPED STATES?
-    { "make_offensive", function(state)
+    { "make_offensive", function (state)
         AddObjective('bdmisn2205.otf',"white");
         state.tracker = mission.UnitTracker:new();
         createWave("svtank",{"spawn_w1"},"west_path"); 
         createWave("svfigh",{"spawn_w4","spawn_w5"},"west_path");
         state:next()
     end },
-    function(state)
+    function (state)
         --Check if got 3 more tanks + 1 bomber, since mission start
         if(state.tracker:gotOfOdf("bvtank",3) and state.tracker:gotOfOdf("bvhraz",1)) then
             state:next();
         end
     end,
-    function(state)
+    function (state)
         UpdateObjective('bdmisn2205.otf',"green");
-        
-        --mission.Objective:Start('make_defensive');
         state:next();
     end,
-    { "make_defensive", function(state)
+    { "make_defensive", function (state)
         AddObjective('bdmisn2206.otf',"white");
         createWave("svtank",{"spawn_w1"},"west_path"); 
         createWave("svfigh",{"spawn_w4","spawn_w5"},"west_path"); -- Original Script did nothing with these 2. Possibly sent to guard Scavs instead? -GBD
         createWave("svscav",{"spawn_w2","spawn_w3"});
         state:next();
     end },
-    function(state)
+    function (state)
         if(tracker:gotOfClass("turrettank",3)) then
             state:next();
         end
     end,
-    function(state)
+    function (state)
         UpdateObjective('bdmisn2206.otf',"green");
-        
-        --mission.Objective:Start('destroy_soviet');
         state:next();
     end,
     -- /SKIPPED STATES?
 
-    {"destroy_soviet", function(state)
+    { "destroy_soviet", function (state)
         createWave("svfigh",{"spawn_e1","spawn_e2"},"east_path");
         createWave("svtank",{"spawn_e3"},"east_path");
         local nav = navmanager.BuildImportantNav(nil, 1, "nav_path", 4);
         nav:SetMaxHealth(0);
-        nav:SetObjectiveName"CCA Base");
+        nav:SetObjectiveName("CCA Base");
         AudioMessage(audio.attack);
         state:next();
     end },
     statemachine.SleepSeconds(45),
-    function(state) -- this one might have been broken before
-        if(not(IsAlive(globals.sb_turr_1) or IsAlive(globals.sb_turr_2))) then
+    function (state) -- this one might have been broken before
+        if not (IsAlive(globals.sb_turr_1) or IsAlive(globals.sb_turr_2)) then
             state:next();
         end
     end,
-    function(state)
+    function (state)
         --UpdateObjective('bdmisn2207.otf',"green");
         SetObjectiveOff(GetRecyclerHandle(2));
 
         --mission.Objective:Start('nsdf_attack');
         state:next();
     end,
-    {"nsdf_attack", function(state)
+    { "nsdf_attack", function (state)
         AudioMessage(audio.nsdf);
         AddObjective('bdmisn2208.otf',"white");
         local a,b,camTarget = createWave("avwalk",{"spawn_avwalk1","spawn_avwalk2","spawn_avwalk3"},"nsdf_path");
@@ -500,24 +493,24 @@ statemachine.Create("main_objectives", {
         for i,v in pairs(state.targets) do
             SetObjectiveOn(v);
         end
-        if(not IsAlive(GetRecyclerHandle(2))) then
+        if not IsAlive(GetRecyclerHandle(2)) then
             UpdateObjective('bdmisn2208.otf',"green"); -- this is odd, this code isn't running anymore right?
         end
         state:next();
     end },
-    function(state)
+    function (state)
         if (state:SecondsHavePassed(10) or CameraPath("camera_nsdf",1000,1500,state.camTarget) or CameraCancelled()) then
             state:SecondsHavePassed(); -- clear timer if we got here without it being cleared
             CameraFinish();
             state:next();
         end
     end,
-    function(state)
+    function (state)
         SetObjectiveOn(GetRecyclerHandle(2));
         UpdateObjective('bdmisn2208.otf',"green");
         state:next();
     end,
-    function(state)
+    function (state)
         if areAllDead(state.targets, 2) then
             state.recycler_target = true;
             SetObjectiveOn(GetRecyclerHandle(2));
@@ -525,23 +518,24 @@ statemachine.Create("main_objectives", {
             state:next();
         end
     end,
-    function(state)
+    function (state)
         if not IsAlive(GetRecyclerHandle(2)) then
             UpdateObjective("bdmisn2207.otf","green");
             state:next();
         end
     end,
-    function(state)
+    function (state)
         AudioMessage(audio.win);
         SucceedMission(GetTime() + 10, "bdmisn22wn.des");
         state:next();
     end
 });
 
+local tug_relic_convoy = statemachine.Start("tug_relic_convoy");
 stateset.Create("mission")
     :Add("main_objectives", statemachine.Start("main_objectives"))
 
-    :Add("destoryNSDF", function(state)
+    :Add("destoryNSDF", function (state)
         if( checkDead(globals.patrolUnits) ) then
             local reinforcements = {
                 BuildObject("svfigh", 2, "spawn_svfigh1"),
@@ -552,16 +546,15 @@ stateset.Create("mission")
             };
             -- Send the reinforcements to Nav 4.
             for i,v in pairs(reinforcements) do
-                Goto(v, GetPosition(globals.navs[4]));
+                Goto(v, GetPosition(globals.nav_research));
             end
             print("Spawning reinforcements");
-            --state:next(); -- move to the next state, which doesn't exist
             state:off("destoryNSDF");
         end
     end)
 
     -- this state never runs?
-    :Add("toofarfrom_recy", function(state)
+    :Add("toofarfrom_recy", function (state)
         if(IsAlive(GetPlayerHandle())) then
             if IsAlive(GetRecyclerHandle(1)) and GetDistance(GetPlayerHandle(), GetRecyclerHandle(1)) > 700.0 then
                 print(state.alive);
@@ -572,19 +565,18 @@ stateset.Create("mission")
     end)
     
     -- Lose conditions by GBD. No idea if i did this right, mission doesn't update otfs, or goto a next thing, it runs throughout the mission. (distance check until ordered to attack CCA base, and recy loss throughout entire mission.)
-    :Add("lose_recy", function(state)
+    :Add("lose_recy", function (state)
         if(not IsAlive(GetRecyclerHandle(1))) then
             FailMission(GetTime() + 5, "bdmisn22l2.des");
             state:off("lose_recy");
         end
 	end)
 
-    :Add("delayed_spawn", statemachine.Start("delayed_spawn"));
+    :Add("delayed_spawn", statemachine.Start("delayed_spawn"))
+    
+    :Add("tug_relic_convoy", tug_relic_convoy);
 
 hook.Add("Start", "Mission:Start", function ()
-    globals.navs = {
-    };
-    globals.currentNav = 1;
     globals.cafe = GetHandle("sbcafe1_i76building");
     globals.comm = GetHandle("sbcomm1_commtower");
     globals.relic = GetHandle("obdata3_artifact");
